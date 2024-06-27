9a1308777c068a5395a87d8d10be4561dfa9fec9
chri9230 <cm@emmetistore.it>
2024-06-27 23:31:41 +0200

    xxx

--- src/components/FormExample.jsx
+++ src/components/FormExample.jsx
@@ -38,13 +38,17 @@ function FormExample() {
       {({ handleSubmit, handleChange, values, touched, errors }) => (
         <Form noValidate onSubmit={handleSubmit}>
           <Row className="mb-3">
+            <h3 className='text-center p-3'>INSERIMENTO NUOVO ARTICOLO</h3>
+
+            {/* CODICE ARTICOLO */}
+
             <Form.Group
               as={Col}
               md="4"
               controlId="validationFormik101"
               className="position-relative"
             >
-              <Form.Label>First name</Form.Label>
+              <Form.Label>CODICE ARTICOLO</Form.Label>
               <Form.Control
                 type="text"
                 name="firstName"
@@ -54,13 +58,39 @@ function FormExample() {
               />
               <Form.Control.Feedback tooltip>Looks good!</Form.Control.Feedback>
             </Form.Group>
+
+            {/* CODICE ARTICOLO FORNITORE */}
+
+            <Form.Group
+              as={Col}
+              md="4"
+              controlId="validationFormik103"
+              className="position-relative"
+            >
+              <Form.Label>CODICE ARTICOLO FORNITORE</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="City"
+                name="city"
+                value={values.city}
+                onChange={handleChange}
+                isInvalid={!!errors.city}
+              />
+
+              <Form.Control.Feedback type="invalid" tooltip>
+                {errors.city}
+              </Form.Control.Feedback>
+            </Form.Group>
+
+            {/* CODICE EAN */}
+
             <Form.Group
               as={Col}
               md="4"
               controlId="validationFormik102"
               className="position-relative"
             >
-              <Form.Label>Last name</Form.Label>
+              <Form.Label>CODICE EAN</Form.Label>
               <Form.Control
                 type="text"
                 name="lastName"
@@ -71,10 +101,13 @@ function FormExample() {
 
               <Form.Control.Feedback tooltip>Looks good!</Form.Control.Feedback>
             </Form.Group>
-            <Form.Group as={Col} md="4" controlId="validationFormikUsername2">
-              <Form.Label>Username</Form.Label>
+
+            {/* DESCRIZIONE ARTICOLO */}
+
+            <Form.Group as={Col} md="12" controlId="validationFormikUsername2">
+              <Form.Label>DESCRIZIONE ARTICOLO</Form.Label>
               <InputGroup hasValidation>
-                <InputGroup.Text id="inputGroupPrepend">@</InputGroup.Text>
+
                 <Form.Control
                   type="text"
                   placeholder="Username"
@@ -90,75 +123,157 @@ function FormExample() {
               </InputGroup>
             </Form.Group>
           </Row>
-          <Row className="mb-3">
-            <Form.Group
-              as={Col}
-              md="6"
-              controlId="validationFormik103"
-              className="position-relative"
-            >
-              <Form.Label>City</Form.Label>
-              <Form.Control
-                type="text"
-                placeholder="City"
-                name="city"
-                value={values.city}
-                onChange={handleChange}
-                isInvalid={!!errors.city}
-              />
 
-              <Form.Control.Feedback type="invalid" tooltip>
-                {errors.city}
-              </Form.Control.Feedback>
-            </Form.Group>
+
+
+          <Row className="mb-3">
+
+
+
+
+
+            {/* PRIMO */}
+
             <Form.Group
               as={Col}
               md="3"
               controlId="validationFormik104"
               className="position-relative"
             >
-              <Form.Label>State</Form.Label>
-              <Form.Control
-                type="text"
-                placeholder="State"
-                name="state"
-                value={values.state}
-                onChange={handleChange}
-                isInvalid={!!errors.state}
-              />
+              <Form.Label>Seleziona l'unità di misura di vendita</Form.Label>
+              <Form.Select aria-label="Default select example">
+
+                <option value="1">Pz</option>
+                <option value="2">Kg</option>
+                <option value="3">Mq</option>
+                <option value="4">Ml</option>
+                <option value="5">Mc</option>
+              </Form.Select>
+
               <Form.Control.Feedback type="invalid" tooltip>
                 {errors.state}
               </Form.Control.Feedback>
             </Form.Group>
+
+            {/* SECONDO */}
+
             <Form.Group
               as={Col}
               md="3"
-              controlId="validationFormik105"
+              controlId="validationFormik104"
               className="position-relative"
             >
-              <Form.Label>Zip</Form.Label>
-              <Form.Control
-                type="text"
-                placeholder="Zip"
-                name="zip"
-                value={values.zip}
-                onChange={handleChange}
-                isInvalid={!!errors.zip}
-              />
+              <Form.Label>Seleziona il Brand</Form.Label>
+              <Form.Select aria-label="Default select example">
+
+                <option value="1">Makita</option>
+                <option value="2">DeWalt</option>
+                <option value="3">Milwaukee</option>
+                <option value="4">Leica</option>
+                <option value="5">Festool</option>
+              </Form.Select>
 
               <Form.Control.Feedback type="invalid" tooltip>
-                {errors.zip}
+                {errors.state}
               </Form.Control.Feedback>
             </Form.Group>
+
+            {/* CATEGORIA */}
+
+            <Form.Group
+              as={Col}
+              md="3"
+              controlId="validationFormik104"
+              className="position-relative"
+            >
+              <Form.Label>Categoria Prodotto</Form.Label>
+              <Form.Select aria-label="Default select example">
+
+                <option value="1">Elettroutensili</option>
+                <option value="2">Ferramenta</option>
+                <option value="3">Isolanti</option>
+                <option value="4">Porte</option>
+                <option value="5">Abbigliamento</option>
+              </Form.Select>
+
+              <Form.Control.Feedback type="invalid" tooltip>
+                {errors.state}
+              </Form.Control.Feedback>
+            </Form.Group>
+
+            {/* PESO */}
+
+            <Form.Group
+              as={Col}
+              md="3"
+              controlId="validationFormik104"
+              className="position-relative"
+            >
+              <Form.Label>Peso (Kg)</Form.Label>
+              <Form.Control
+                type="number"
+                name="productCategory"
+                id="productCategory"
+                value={values.productCategory}
+                onChange={handleChange}
+                min="1"   
+                step="5"
+              />
+            </Form.Group>
+
+
+
           </Row>
+
+          <Row>
+
+          <Form.Group
+              as={Col}
+              md="3"
+              controlId="validationFormik104"
+              className="position-relative"
+            >
+              <Form.Label>Prezzo d'acquisto (€)</Form.Label>
+              <Form.Control
+                type="number"
+                name="productCategory"
+                id="productCategory"
+                value={values.productCategory}
+                onChange={handleChange}
+                min="1"   
+                step="1"
+              />
+            </Form.Group>
+
+            <Form.Group
+              as={Col}
+              md="3"
+              controlId="validationFormik104"
+              className="position-relative"
+            >
+              <Form.Label>Prezzo d'acquisto Medio (€)</Form.Label>
+              <Form.Control
+                type="number"
+                name="productCategory"
+                id="productCategory"
+                value={values.productCategory}
+                onChange={handleChange}
+                min="1"   
+                step="1"
+              />
+            </Form.Group>
+          </Row>
+
+
+
           <Form.Group className="position-relative mb-3">
-            <Form.Label>File</Form.Label>
+            <Form.Label>Aggiungi un immagine prodotto</Form.Label>
             <Form.Control
               type="file"
               required
               name="file"
               onChange={handleChange}
-              isInvalid={!!errors.file}
+              // isInvalid={!!errors.file}
             />
             <Form.Control.Feedback type="invalid" tooltip>
               {errors.file}
